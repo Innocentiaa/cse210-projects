d9367f9482d28fbb2685d5452a14cc7f4bd5dc75
Innocentiaa <eke24009@byui.edu>
2025-05-13 20:23:35 +0100

    Week one Coursework

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain;
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            
+            int guess = -1;
+            int attempts = 0;
+
+            
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                
+                attempts++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You guessed it in {attempts} attempts");
+                }
+            }
+            
+            Console.Write("Do you want to play again? (yes/no):");
+            playAgain = Console.ReadLine();
+        }while (playAgain == "yes");
+        Console.WriteLine("Thanks for playing");
     }
 }
