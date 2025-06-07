37438bca028c7dbe5967a6951bfc804fbc8bdfa3
Innocentiaa <eke24009@byui.edu>
2025-06-08 00:49:05 +0100

    MathAssignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment a1 = new Assignment("Gwendolyn", "Division");
+        Console.WriteLine(a1.GetSummary());
+
+        // Now create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Anthonia", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Chris Emeka", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
